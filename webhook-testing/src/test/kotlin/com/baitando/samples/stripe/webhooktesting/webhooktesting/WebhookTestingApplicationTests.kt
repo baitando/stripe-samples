a8fc3e5600aa75d59044dd0915f89761594140e5
Andreas Hirsch <andreas.hirsch@baitando.com>
2021-09-22 23:34:26 +0200

    Add webhook testing sample

--- webhook-testing/src/test/kotlin/com/baitando/samples/stripe/webhooktesting/webhooktesting/WebhookTestingApplicationTests.kt
+++ webhook-testing/src/test/kotlin/com/baitando/samples/stripe/webhooktesting/webhooktesting/WebhookTestingApplicationTests.kt
@@ -2,8 +2,10 @@ package com.baitando.samples.stripe.webhooktesting.webhooktesting
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(properties = ["sample.stripe.key: dummy"])
 class WebhookTestingApplicationTests {
 
     @Test
